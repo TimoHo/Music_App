2d2ddb4ceda7c14c8f1fcbf7cbcdc9e3e68eaeba
Timo Hocker <Timo Hocker>
2016-05-25 12:41:01 +0200

    Update

--- src/me/tmods/app/music/DisplaySound.java
+++ src/me/tmods/app/music/DisplaySound.java
@@ -1,6 +1,7 @@
 package me.tmods.app.music;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,28 +10,53 @@ import java.util.List;
 public class DisplaySound extends Canvas{
 	private List<Integer> sounds = new ArrayList<Integer>();
 	private List<Integer> pos = new ArrayList<Integer>();
+	private Boolean stuck = false;
 	public DisplaySound() {}
 	public DisplaySound update() throws InstantiationException, IllegalAccessException {
-		for (int i = 0;i<sounds.size();i++) {
-			pos.set(i,pos.get(i) + 5);
-			if (pos.get(i) > this.getWidth()) {
-				sounds.remove(i);
-				pos.remove(i);
+		if (this.stuck) {
+			return this;
+		}
+		try {
+			for (int i = 0;i<sounds.size();i++) {
+				pos.set(i,pos.get(i) + 5);
+				if (pos.get(i) > this.getWidth()) {
+					sounds.remove(i);
+					pos.remove(i);
+				}
 			}
+		} catch (Exception e) {
+			this.stuck = true;
 		}
 		return this;
 	}
 	public DisplaySound add(Integer height) {
-		sounds.add(height * 2);
-		pos.add(0);
+		if (this.stuck) {
+			return this;
+		}
+		try {
+			sounds.add(height * 2);
+			pos.add(0);
+		} catch (Exception e) {
+			this.stuck = true;
+		}
 		return this;
 	}
 	@Override
 	public void paint(Graphics g) {
-		for(int i = 0;i<sounds.size();i++) {
-			if (i != 0) {
-				g.drawLine(this.getWidth() - pos.get(i-1), this.getHeight() - sounds.get(i-1),this.getWidth() - pos.get(i), this.getHeight() - sounds.get(i));
+		if (this.stuck) {
+			g.setColor(Color.RED);
+			g.drawLine(0, 0, this.getWidth(), this.getHeight());
+			g.drawLine(this.getWidth(), 0, 0, this.getHeight());
+			return;
+		}
+		try {
+			for(int i = 0;i<sounds.size();i++) {
+				if (i != 0) {
+					g.drawLine(this.getWidth() - pos.get(i-1), this.getHeight() - sounds.get(i-1),this.getWidth() - pos.get(i), this.getHeight() - sounds.get(i));
+				}
 			}
+		} catch (Exception e) {
+			this.stuck = true;
 		}
 	}
 }
